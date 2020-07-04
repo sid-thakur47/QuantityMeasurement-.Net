f0c51d3b4780be8fdacbe52ede47848561a7b038
sid-thakur47 <sidthakur258@gmail.com>
2020-07-04 17:04:20 +0530

    ADDED:Functionality to compare Feet

---
+++ QuantityMeasurement/Length.cs
@@ -0,0 +1,98 @@
+﻿
+//-----------------------------------------------------------------------
+// <copyright file="Length.cs" company="BridgeLabz">
+// Copyright (c) 2020 All Rights Reserved
+// </copyright>
+//-----------------------------------------------------------------------
+namespace QuantityMeasurement.BridgeLabz
+{
+#pragma warning disable CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
+    public class Length
+#pragma warning restore CS0659 // Type overrides Object.Equals(object o) but does not override Object.GetHashCode()
+    {
+       /// <summary>
+       /// Instance of Unit
+       /// </summary>
+       public Unit UnitType;
+        /// <summary>
+        /// value to be converted
+        /// </summary>
+       private double Value;
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Length"/> class
+        /// </summary>
+        public Length()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Length"/> class
+        /// </summary>
+        /// <param name="unit">Unit to be converted</param>
+        /// <param name="value">value of unit</param>
+        public Length(Unit unit, double value)
+        {
+            this.UnitType = unit;
+            this.Value = value;
+        }
+
+        public enum Unit
+        {
+            /// <summary>
+            /// comaparing feet
+            /// </summary>
+            FEET,
+
+            /// <summary>
+            /// comparing inch
+            /// </summary>
+            INCH
+        };
+
+        /// <summary>
+        /// Overriding Equals Function.
+        /// </summary>
+        /// <param name="obj">Object for comparing</param>
+        /// <returns>Boolean after equality check</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            { 
+                return false;
+            }
+
+            if (obj == this)
+            {
+                return true;
+            }
+
+            Length that = (Length)obj;
+            return (this.UnitType == that.UnitType) && (this.Value == that.Value) ||
+                 (this.UnitType.Equals(Unit.FEET) && that.UnitType.Equals(Unit.INCH) &&
+                this.Value == 0 && that.Value == 0);
+        }
+
+        /// <summary>
+        /// For conversion of feet to inch
+        /// </summary>
+        /// <param name="valueInFeet"> value in feet</param>
+        /// <returns> feet converion to inch</returns>
+        public double FeetToInch(double valueInFeet)
+        {
+            return valueInFeet * 12;
+        }
+
+        /// <summary>
+        /// For conversion of inch to feet
+        /// </summary>
+        /// <param name="valueInInch"> value in inch</param>
+        /// <returns>inch conversion to feet</returns>
+        public double InchToFeet(double valueInInch)
+        {
+            return valueInInch / 12;
+        }
+    }
+}
--- QuantityMeasurementTest/QuantityMeasuremnetTest.cs
+++ QuantityMeasurementTest/QuantityMeasuremnetTest.cs
@@ -1,18 +1,72 @@
+//-----------------------------------------------------------------------
+// <copyright file="QuantityMeasurementTest.cs" company="BridgeLabz">
+// Copyright (c) 2020 All Rights Reserved
+// </copyright>
+//------------------------------------------------------------------
+
+namespace QuantityMeasurement.BridgeLabz
+{
 using NUnit.Framework;
 
-namespace QuantityMeasurementTest
-{
-    public class Tests
+    /// <summary>
+    /// Test for Quantity Measurement
+    /// </summary>
+    public class QuantityMeasurementTest
     {
-        [SetUp]
-        public void Setup()
+        /// <summary>
+        /// Test case 1.1: Given zero feet for comparing should return true 
+        /// </summary>
+        [Test]
+        public void GivenZeroFeet_When_ZeroFeet_ShouldReturnZero()
         {
+            Length firstFeet = new Length(Length.Unit.FEET, 0.0);
+            Length secondFeet = new Length(Length.Unit.FEET, 0.0);
+            bool equals = firstFeet.Equals(secondFeet);
+            Assert.IsTrue(equals);
         }
 
+        /// <summary>
+        /// Test case 1.2: Given Null feet should return false
+        /// </summary>
         [Test]
-        public void Test1()
-        { 
-            Assert.Pass();
+        public void GivenFeet_When_Null_ShouldReturnFalse()
+        {
+            bool equals = Length.Equals(Length.Unit.FEET, null);
+            Assert.IsFalse(equals);
+        }
+
+        /// <summary>
+        /// Test case 1.3: Given Null feet should return false
+        /// </summary>
+        [Test]
+        public void GivenFeet_When_SameReference_ShouldReturnTrue()
+        {
+            Length firstReferanceFeet = new Length();
+            Length secondReferanceFeet = new Length();
+            Assert.AreEqual(firstReferanceFeet, secondReferanceFeet);
+        }
+
+        /// <summary>
+        /// Test case 1.4: Given Null feet should return false
+        /// </summary>
+        [Test]
+        public void GivenFeet_When_Type_EqualToReturnTrue()
+        {
+            Length firstFeetValue = new Length(Length.Unit.FEET, 0.0);
+            Length secondFeetValue = new Length(Length.Unit.FEET, 0.0);
+            Assert.AreEqual(firstFeetValue.GetType(), secondFeetValue.GetType());
+        }
+
+        /// <summary>
+        /// Test case 1.5: Feet with same value should return true
+        /// </summary>
+        [Test]
+        public void GivenFeet_When_SameValue_ShouldReturnEqual()
+        {
+            Length firstFeetValue = new Length(Length.Unit.FEET, 10.0);
+            Length secondFeetValue = new Length(Length.Unit.FEET, 10.0);
+            bool equals = firstFeetValue.Equals(secondFeetValue);
+            Assert.True(equals);
         }
     }
 }

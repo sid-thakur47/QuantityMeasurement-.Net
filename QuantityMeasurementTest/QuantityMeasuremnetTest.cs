3adecb9373ce8d505960b7855e24017843dab333
sid-thakur47 <sidthakur258@gmail.com>
2020-07-04 17:15:31 +0530

    ADDED:Functionality to compare Inch

--- QuantityMeasurementTest/QuantityMeasuremnetTest.cs
+++ QuantityMeasurementTest/QuantityMeasuremnetTest.cs
@@ -8,11 +8,14 @@ namespace QuantityMeasurement.BridgeLabz
 {
 using NUnit.Framework;
 
+
     /// <summary>
     /// Test for Quantity Measurement
     /// </summary>
     public class QuantityMeasurementTest
     {
+        Length length = new Length();
+
         /// <summary>
         /// Test case 1.1: Given zero feet for comparing should return true 
         /// </summary>
@@ -67,6 +70,62 @@ using NUnit.Framework;
             Length secondFeetValue = new Length(Length.Unit.FEET, 10.0);
             bool equals = firstFeetValue.Equals(secondFeetValue);
             Assert.True(equals);
+        }
+
+        /// <summary>
+        /// Test case 1.6: Given  Inch when zero should return true
+        /// </summary>
+        [Test]
+        public void GivenInch_When_Zero_ShouldReturnFalse()
+        {
+            Length firstInch = new Length(Length.Unit.INCH, 0.0);
+            Length secondInch = new Length(Length.Unit.INCH, 0.0);
+            bool equals = firstInch.Equals(secondInch);
+            Assert.IsTrue(equals);
+        }
+
+        /// <summary>
+        /// Test case 1.7: Given Inch when Null should return false
+        /// </summary>
+        [Test]
+        public void GivenInch_When_Null_ShouldReturnFalse()
+        {
+            bool equals = Length.Equals(Length.Unit.INCH, null);
+            Assert.IsFalse(equals);
+        }
+
+        /// <summary>
+        /// Test case 1.8: Given Inch with same reference  should return true
+        /// </summary>
+        [Test]
+        public void GivenInch_When_SameReference_ShouldReturnTrue()
+        {
+            //Instance of Conversion
+            Length firstReferanceInch = new Length();
+            Length secondReferanceInch = firstReferanceInch;
+            Assert.AreEqual(firstReferanceInch, secondReferanceInch);
+        }
+
+        /// <summary>
+        /// Test case 1.9: Given Inch with same type should return true
+        /// </summary>
+        [Test]
+        public void GivenInch_When_SameType_ShouldReturnTrue()
+        {
+            Length firstInchValue = new Length(Length.Unit.INCH, 0.0);
+            Length secondInchValue = new Length(Length.Unit.INCH, 0.0);
+            Assert.AreEqual(firstInchValue.GetType(), secondInchValue.GetType());
+        }
+
+        /// <summary>
+        /// Test case 1.10: Given Inch with same value should return true
+        /// </summary>
+        [Test]
+        public void GivenInch_When_SameValue_ShouldReturnTrue()
+        {
+            Length firstInchValue = new Length(Length.Unit.INCH, 10.0);
+            Length secondInchValue = new Length(Length.Unit.INCH, 10.0);
+            Assert.AreEqual(firstInchValue.GetType(), secondInchValue.GetType());
         }
     }
 }
